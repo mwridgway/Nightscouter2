36567e3a59fe35952e91aa8859ec1202e6331e0d
Peter Ina <peter.ina@gmail.com>
2016-01-19 22:26:43 -0500

    Temp Datasource

---
+++ Common/SitesDataSource.swift
@@ -0,0 +1,36 @@
+//
+//  SitesDataSource.swift
+//  Nightscouter
+//
+//  Created by Peter Ina on 1/15/16.
+//  Copyright © 2016 Nothingonline. All rights reserved.
+//
+
+import Foundation
+
+public protocol SitesDataSourceProvider: Dateable {
+    var sites: [Site] { get }
+    var lastViewedSiteIndex: Int { get }
+}
+
+extension SitesDataSourceProvider {
+    public var milliseconds: Double {
+        return 1168583640000
+    }
+    public var lastViewedSiteIndex: Int {
+        return 0
+    }
+}
+
+public class SitesDataSource: SitesDataSourceProvider{
+    public var sites: [Site]
+    public var lastViewedSiteIndex = 0
+    public var lastViewedSiteUUID: NSUUID?
+    public var siteForComplication: NSUUID?
+    public var milliseconds: Double = AppConfiguration.Constant.knownMilliseconds
+    
+    public static let sharedInstance = SitesDataSource()
+    private init(){
+        sites = [Site(url: NSURL(string: "")!, apiSecret: "")]
+    }
+}
