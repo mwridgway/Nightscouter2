d106d84f3a4f6141d32cbf1a99bdd33bb0ac070a
Peter Ina <peter.ina@gmail.com>
2016-01-20 19:04:16 -0500

    More keys...

--- Common/JSONParser.swift
+++ Common/JSONParser.swift
@@ -10,7 +10,7 @@ import Foundation
 import SwiftyJSON
 
 // All the JSON keys I saw when parsing the socket.io output for dataUpdate
-struct jp {
+struct JSONPropertyKey {
     static let lastUpdated = "lastUpdated"
     static let devicestatus = "devicestatus"
     static let uploader = "uploader"
@@ -33,17 +33,12 @@ struct jp {
     static let profiles = "profiles"
     static let treatments = "treatments"
     static let deltaCount = "delta"
-    
     static let url = "url"
     static let overrideScreenLock = "overrideScreenLock"
     static let disabled = "disabled"
     static let uuid = "uuid"
-    
     static let configurationJSON = "configurationJSON"
     static let socketJSON = "socketJSON"
-    
-    
-    
     static let status = "status"
     static let name = "name"
     static let version = "version"
@@ -52,45 +47,60 @@ struct jp {
     static let careportalEnabled = "careportalEnabled"
     static let boluscalcEnabled = "boluscalcEnabled"
     static let head = "head"
-
     static let settings = "settings"
     static let units = "units"
     static let timeFormat = "timeFormat"
     static let nightMode = "nightMode"
     static let editMode = "editMode"
     static let showRawbg = "showRawbg"
-    
     static let customTitle = "customTitle"
     static let theme = "theme"
     static let alarmUrgentHigh = "alarmUrgentHigh"
     static let alarmHigh = "alarmHigh"
     static let alarmLow = "alarmLow"
     static let alarmUrgentLow = "alarmUrgentLow"
-
+    static let alarmUrgentHighMins = "alarmUrgentHighMins"
+    static let alarmHighMins = "alarmHighMins"
+    static let alarmLowMins = "alarmLowMins"
+    static let alarmUrgentLowMins = "alarmUrgentLowMins"
+    static let alarmWarnMins = "alarmWarnMins"
+    static let alarmTimeagoWarn = "alarmTimeagoWarn"
+    static let alarmTimeagoWarnMins = "alarmTimeagoWarnMins"
+    static let alarmTimeagoUrgent = "alarmTimeagoUrgent"
+    static let alarmTimeagoUrgentMins = "alarmTimeagoUrgentMins"
+    static let language = "language"
+    static let scaleY = "scaleY"
+    static let enable = "enable"
+    static let thresholds = "thresholds"
+    static let bgHigh = "bgHigh"
+    static let bgLow = "bgLow"
+    static let bgTargetBottom = "bgTargetBottom"
+    static let bgTargetTop = "bgTargetTop"
+    static let alarmType = "alarmType"
+    static let heartbeat = "heartbeat"
+    static let baseURL = "baseURL"
 }
-
 
 extension Site {
     
     func siteJSON(config: JSON?, socket: JSON?) -> JSON {
         
         guard let config = config, socket = socket else {
-            return [jp.url : url, jp.overrideScreenLock : overrideScreenLock, jp.disabled: disabled, jp.uuid: uuid]
+            return [JSONPropertyKey.url : url, JSONPropertyKey.overrideScreenLock : overrideScreenLock, JSONPropertyKey.disabled: disabled, JSONPropertyKey.uuid: uuid]
         }
         
-        let json: JSON = [jp.url : url, jp.overrideScreenLock : overrideScreenLock, jp.disabled: disabled, jp.uuid: uuid, jp.configurationJSON: config.dictionaryObject!, jp.socketJSON: socket.dictionaryObject!]
+        let json: JSON = [JSONPropertyKey.url : url, JSONPropertyKey.overrideScreenLock : overrideScreenLock, JSONPropertyKey.disabled: disabled, JSONPropertyKey.uuid: uuid, JSONPropertyKey.configurationJSON: config.dictionaryObject!, JSONPropertyKey.socketJSON: socket.dictionaryObject!]
         
         return json
     }
     
-    
     mutating func parseJSONforSite(json: JSON) {
-        let url = json[jp.url].URL ?? NSURL()
-        let overrideScreenLock = json[jp.overrideScreenLock].bool ?? false
-        let disabled = json[jp.disabled].bool ?? false
-        let configurationJSON = json[jp.configurationJSON] ?? JSON(["error" : "configurationJSON"])
-        let socketJSON = json[jp.socketJSON] ??  JSON(["error" : "socketJSON"])
-        let uuid = NSUUID(UUIDString: json[jp.uuid].stringValue) ?? NSUUID()
+        let url = json[JSONPropertyKey.url].URL ?? NSURL()
+        let overrideScreenLock = json[JSONPropertyKey.overrideScreenLock].bool ?? false
+        let disabled = json[JSONPropertyKey.disabled].bool ?? false
+        let configurationJSON = json[JSONPropertyKey.configurationJSON] ?? JSON(["error" : "configurationJSON"])
+        let socketJSON = json[JSONPropertyKey.socketJSON] ??  JSON(["error" : "socketJSON"])
+        let uuid = NSUUID(UUIDString: json[JSONPropertyKey.uuid].stringValue) ?? NSUUID()
         
         
         self.url = url
@@ -102,54 +112,48 @@ extension Site {
         parseJSONforSocketData(socketJSON)
     }
     
-
     mutating func parseJSONforConfiugration(json: JSON) {
         
-        let settingsJSON = json[jp.settings]
-        
-        let units = Units(rawValue: settingsJSON[jp.units].stringValue) ?? .Mmol
-        let timeFormat = settingsJSON[jp.timeFormat].int ?? 12
-        let nightMode = settingsJSON[jp.nightMode].bool ?? false
-        let editMode = settingsJSON[jp.editMode].bool ?? true
-        let showRawbg = RawBGMode(rawValue: settingsJSON[jp.showRawbg].stringValue) ?? .Never
-        let customtitle = settingsJSON[jp.customTitle].string ?? "Unknown"
-        let theme = settingsJSON[jp.theme].string ?? "color"
-        
-        
-        let alarmUrgentHigh = settingsJSON[jp.alarmUrgentHigh].bool ?? true
-        let alarmHigh = settingsJSON[jp.alarmHigh].bool ?? true
-        let alarmLow = settingsJSON[jp.alarmLow].bool ?? true
-        let alarmUrgentLow = settingsJSON[jp.alarmUrgentLow].bool ?? true
-        
+        let settingsJSON = json[JSONPropertyKey.settings]
+        let units = Units(rawValue: settingsJSON[JSONPropertyKey.units].stringValue) ?? .Mmol
+        let timeFormat = settingsJSON[JSONPropertyKey.timeFormat].int ?? 12
+        let nightMode = settingsJSON[JSONPropertyKey.nightMode].bool ?? false
+        let editMode = settingsJSON[JSONPropertyKey.editMode].bool ?? true
+        let showRawbg = RawBGMode(rawValue: settingsJSON[JSONPropertyKey.showRawbg].stringValue) ?? .Never
+        let customtitle = settingsJSON[JSONPropertyKey.customTitle].string ?? "Unknown"
+        let theme = settingsJSON[JSONPropertyKey.theme].string ?? "color"
+        let alarmUrgentHigh = settingsJSON[JSONPropertyKey.alarmUrgentHigh].bool ?? true
+        let alarmHigh = settingsJSON[JSONPropertyKey.alarmHigh].bool ?? true
+        let alarmLow = settingsJSON[JSONPropertyKey.alarmLow].bool ?? true
+        let alarmUrgentLow = settingsJSON[JSONPropertyKey.alarmUrgentLow].bool ?? true
         let placeholderAlarm1 = [15, 30, 45, 60]
         let placeholderAlarm2 = [30, 60, 90, 120]
+        let alarmUrgentHighMins: [Int] = settingsJSON[JSONPropertyKey.alarmUrgentHighMins].arrayValue.map { $0.int! } ?? placeholderAlarm2
+        let alarmHighMins: [Int] = settingsJSON[JSONPropertyKey.alarmHighMins].arrayValue.map { $0.int! } ?? placeholderAlarm2
+        let alarmLowMins: [Int] = settingsJSON[JSONPropertyKey.alarmLowMins].arrayValue.map { $0.int! } ?? placeholderAlarm1
+        let alarmUrgentLowMins: [Int] = settingsJSON[JSONPropertyKey.alarmUrgentLowMins].arrayValue.map { $0.int! } ?? placeholderAlarm1
         
-        let alarmUrgentHighMins: [Int] = settingsJSON["alarmUrgentHighMins"].arrayValue.map { $0.int! } ?? placeholderAlarm2
-        let alarmHighMins: [Int] = settingsJSON["alarmHighMins"].arrayValue.map { $0.int! } ?? placeholderAlarm2
-        let alarmLowMins: [Int] = settingsJSON["alarmLowMins"].arrayValue.map { $0.int! } ?? placeholderAlarm1
-        let alarmUrgentLowMins: [Int] = settingsJSON["alarmUrgentLowMins"].arrayValue.map { $0.int! } ?? placeholderAlarm1
+        let alarmWarnMins: [Int] = settingsJSON[JSONPropertyKey.alarmWarnMins].arrayValue.map { $0.int! } ?? placeholderAlarm2
         
-        let alarmWarnMins: [Int] = settingsJSON["alarmWarnMins"].arrayValue.map { $0.int! } ?? placeholderAlarm2
+        let alarmTimeagoWarn = settingsJSON[JSONPropertyKey.alarmTimeagoWarn].bool ?? true
+        let alarmTimeagoWarnMins = (settingsJSON[JSONPropertyKey.alarmTimeagoWarnMins].double ?? 15) * 60
         
-        let alarmTimeagoWarn = settingsJSON["alarmTimeagoWarn"].bool ?? true
-        let alarmTimeagoWarnMins = (settingsJSON["alarmTimeagoWarnMins"].double ?? 15) * 60
+        let alarmTimeagoUrgent = settingsJSON[JSONPropertyKey.alarmTimeagoUrgent].bool ?? true
+        let alarmTimeagoUrgentMins = (settingsJSON[JSONPropertyKey.alarmTimeagoUrgentMins].double ?? 30) * 60
         
-        let alarmTimeagoUrgent = settingsJSON["alarmTimeagoUrgent"].bool ?? true
-        let alarmTimeagoUrgentMins = (settingsJSON["alarmTimeagoUrgentMins"].double ?? 30) * 60
+        let language = settingsJSON[JSONPropertyKey.language].string ?? "en"
+        let scaleY = settingsJSON[JSONPropertyKey.scaleY].string ?? "log"
         
-        let language = settingsJSON["language"].string ?? "en"
-        let scaleY = settingsJSON["scaleY"].string ?? "log"
+        let enabled: [ShowPlugins] = settingsJSON[JSONPropertyKey.enable].arrayValue.flatMap {
+            ShowPlugins(rawValue: $0.stringValue)
+        }
         
+        let thresholdsJSON = settingsJSON[JSONPropertyKey.thresholds]
+        let thresholds = Thresholds(bgHigh: thresholdsJSON[JSONPropertyKey.bgHigh].doubleValue, bgLow: thresholdsJSON[JSONPropertyKey.bgLow].doubleValue, bgTargetBottom: thresholdsJSON[JSONPropertyKey.bgTargetBottom].doubleValue, bgTargetTop: thresholdsJSON[JSONPropertyKey.bgTargetTop].doubleValue)
         
-        let enabled: [ShowPlugins] = settingsJSON["enable"].arrayValue.flatMap { ShowPlugins(rawValue: $0.stringValue) }
-        
-        
-        let thresholdsJSON = settingsJSON["thresholds"]
-        let thresholds = Thresholds(bgHigh: thresholdsJSON["bgHigh"].doubleValue, bgLow: thresholdsJSON["bgLow"].doubleValue, bgTargetBottom: thresholdsJSON["bgTargetBottom"].doubleValue, bgTargetTop: thresholdsJSON["bgTargetTop"].doubleValue)
-        
-        let alarmType = AlarmType(rawValue: settingsJSON["alarmType"].stringValue) ?? .simple
-        let hreartbeat = settingsJSON["heartbeat"].int ?? 60
-        let baseURL = settingsJSON["baseURL"].stringValue
+        let alarmType = AlarmType(rawValue: settingsJSON[JSONPropertyKey.alarmType].stringValue) ?? .simple
+        let hreartbeat = settingsJSON[JSONPropertyKey.heartbeat].int ?? 60
+        let baseURL = settingsJSON[JSONPropertyKey.baseURL].stringValue
         
         let timeAgo = TimeAgoAlert(warn: alarmTimeagoWarn, warnMins: alarmTimeagoWarnMins, urgent: alarmTimeagoUrgent, urgentMins: alarmTimeagoUrgentMins)
         
@@ -157,71 +161,60 @@ extension Site {
         
         let settings = Settings(units: units, timeFormat: timeFormat, nightMode: nightMode, editMode: editMode, showRawbg: showRawbg, customTitle: customtitle, theme: theme, alarms: alarms, timeAgo: timeAgo, scaleY: scaleY, language: language, showPlugins: enabled, enable: enabled, thresholds: thresholds, baseURL: baseURL, alarmType: alarmType, heartbeat: hreartbeat)
         
-        let serverConfiguration = ServerConfiguration(status: json[jp.status].stringValue, version: json[jp.version].stringValue, name: json[jp.name].stringValue, serverTime: json[jp.serverTime].stringValue, api: json[jp.apiEnabled].boolValue, carePortal: json[jp.careportalEnabled].boolValue , boluscalc: json[jp.boluscalcEnabled].boolValue, settings: settings, head: json[jp.head].stringValue)
+        let serverConfiguration = ServerConfiguration(status: json[JSONPropertyKey.status].stringValue, version: json[JSONPropertyKey.version].stringValue, name: json[JSONPropertyKey.name].stringValue, serverTime: json[JSONPropertyKey.serverTime].stringValue, api: json[JSONPropertyKey.apiEnabled].boolValue, carePortal: json[JSONPropertyKey.careportalEnabled].boolValue , boluscalc: json[JSONPropertyKey.boluscalcEnabled].boolValue, settings: settings, head: json[JSONPropertyKey.head].stringValue)
+        
         self.configuration = serverConfiguration
     }
     
     mutating func parseJSONforSocketData(json: JSON) {
         
-        if let lastUpdated = json[jp.lastUpdated].double {
-            // print(lastUpdated)
+        if let lastUpdated = json[JSONPropertyKey.lastUpdated].double {
             self.milliseconds = lastUpdated
         }
         
-        if let uploaderBattery = json[jp.devicestatus][jp.uploaderBattery].int {
+        if let uploaderBattery = json[JSONPropertyKey.devicestatus][JSONPropertyKey.uploaderBattery].int {
             self.deviceStatus.append(DeviceStatus(uploaderBattery: uploaderBattery, milliseconds: 0))
         }
         
-        let deviceStatus = json[jp.devicestatus]
-        
+        let deviceStatus = json[JSONPropertyKey.devicestatus]
         for (_, subJson) in deviceStatus {
             print(subJson.description)
-            if let mills = subJson[jp.mills].double {
-                if let uploaderBattery = subJson[jp.uploader, jp.battery].int {
+            if let mills = subJson[JSONPropertyKey.mills].double {
+                if let uploaderBattery = subJson[JSONPropertyKey.uploader, JSONPropertyKey.battery].int {
                     self.deviceStatus.append(DeviceStatus(uploaderBattery: uploaderBattery, milliseconds: mills))
                 }
             }
         }
-    
-        let sgvs = json[jp.sgvs]
+        
+        let sgvs = json[JSONPropertyKey.sgvs]
         for (_, subJson) in sgvs {
-            if let deviceString = subJson[jp.device].string, rssi = subJson[jp.rssi].int, unfiltered = subJson[jp.unfiltered].double, directionString = subJson[jp.direction].string, filtered = subJson[jp.filtered].double, noiseInt = subJson[jp.noise].int, mills = subJson[jp.mills].double, mgdl = subJson[jp.mgdl].double {
+            if let deviceString = subJson[JSONPropertyKey.device].string, rssi = subJson[JSONPropertyKey.rssi].int, unfiltered = subJson[JSONPropertyKey.unfiltered].double, directionString = subJson[JSONPropertyKey.direction].string, filtered = subJson[JSONPropertyKey.filtered].double, noiseInt = subJson[JSONPropertyKey.noise].int, mills = subJson[JSONPropertyKey.mills].double, mgdl = subJson[JSONPropertyKey.mgdl].double {
                 
                 let device = Device(rawValue: deviceString) ?? Device.Unknown
                 let direction = Direction(rawValue: directionString) ?? Direction.None
                 let noise = Noise(rawValue: noiseInt) ?? Noise()
                 
                 let sensorValue = SensorGlucoseValue(direction: direction, device: device, rssi: rssi, unfiltered: unfiltered, filtered: filtered, mgdl: mgdl, noise: noise, milliseconds: mills)
-                
-                
                 self.sgvs.append(sensorValue)
-                // print(sensorValue)
             }
         }
         
-        let mbgs = json[jp.mbgs]
+        let mbgs = json[JSONPropertyKey.mbgs]
         for (_, subJson) in mbgs {
-            if let deviceString = subJson[jp.device].string, mills = subJson[jp.mills].double, mgdl = subJson[jp.mgdl].double {
+            if let deviceString = subJson[JSONPropertyKey.device].string, mills = subJson[JSONPropertyKey.mills].double, mgdl = subJson[JSONPropertyKey.mgdl].double {
                 let device = Device(rawValue: deviceString) ?? Device.Unknown
-                
                 let meter = MeteredGlucoseValue(milliseconds: mills, device: device, mgdl: mgdl)
                 self.mbgs.append(meter)
-                // print(meter)
             }
         }
         
-        let cals = json[jp.cals]
+        let cals = json[JSONPropertyKey.cals]
         for (_, subJson) in cals {
-            if let slope = subJson[jp.slope].double, intercept = subJson[jp.intercept].double, scale = subJson[jp.scale].double, mills = subJson[jp.mills].double {
-                
+            if let slope = subJson[JSONPropertyKey.slope].double, intercept = subJson[JSONPropertyKey.intercept].double, scale = subJson[JSONPropertyKey.scale].double, mills = subJson[JSONPropertyKey.mills].double {
                 let calibration = Calibration(slope: slope, intercept: intercept, scale: scale, milliseconds: mills)
-                
                 self.cals.append(calibration)
-                // print(calibration)
             }
         }
-        // print(site)
-        
         // makes sure things are sorted correctly by date. When delta's come in they might screw up the order.
         self.sgvs.sortInPlace{(item1:SensorGlucoseValue, item2:SensorGlucoseValue) -> Bool in
             item1.date.compare(item2.date) == NSComparisonResult.OrderedDescending
