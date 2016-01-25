185105e336a748e31bee139988ec5e3928c87619
Peter Ina <peter.ina@gmail.com>
2016-01-25 16:02:05 -0500

    messing around with updates to sites data source

--- Common/SitesDataSource.swift
+++ Common/SitesDataSource.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import SwiftyJSON
+//import SwiftyJSON
 
 public protocol SitesDataSourceProvider: Dateable {
     var sites: [Site] { get }
@@ -24,51 +24,84 @@ extension SitesDataSourceProvider {
 }
 
 public class SitesDataSource: SitesDataSourceProvider{
-    public var sites: [Site]
-    public var lastViewedSiteIndex = 0
-    public var lastViewedSiteUUID: NSUUID?
+    public var sites = [Site]()
+    public var lastViewedSiteIndex: Int? {
+        didSet {
+            saveSitesToDefaults()
+        }
+    }
+    public var lastViewedSiteUUID: NSUUID? {
+        didSet {
+            saveSitesToDefaults()
+        }
+    }
     public var siteForComplication: NSUUID?
     //public var milliseconds: Double = AppConfiguration.Constant.knownMilliseconds
     
     public static let sharedInstance = SitesDataSource()
-    private init(){
-        let userDefaults = NSUserDefaults.standardUserDefaults()
-        if let sites = userDefaults.valueForKey("sites") as? [[String: AnyObject]] {
-            self.sites = sites.flatMap { Site().decode($0) }
-        } else {
-            // add default data
-            sites = []
-        }
+    
+    public let defaults =  NSUserDefaults.standardUserDefaults()
+    
+    enum DefaultKey: String, RawRepresentable {
+        case sites,lastViewedSiteIndex,lastViewedSiteUUID, siteForComplication
     }
     
+    private init(){
+        loadSitesFromDefaults()
+    }
     
     // MARK: Persistence
     
-    public func archiveSites() {
+    public func saveSitesToDefaults() {
         let siteDict = sites.map { $0.encode() }
+        defaults.setObject(siteDict, forKey: DefaultKey.sites.rawValue)
         
-        NSUserDefaults.standardUserDefaults().setObject(siteDict, forKey: "sites")
-        NSUserDefaults.standardUserDefaults().synchronize()
+        defaults.setInteger(lastViewedSiteIndex, forKey: DefaultKey.lastViewedSiteIndex.rawValue)
+        defaults.setObject(lastViewedSiteUUID?.UUIDString, forKey: DefaultKey.lastViewedSiteUUID.rawValue)
+        defaults.setObject(siteForComplication?.UUIDString, forKey: DefaultKey.siteForComplication.rawValue)
+        
+        defaults.synchronize()
     }
     
+    public func loadSitesFromDefaults() {
+        if let sites = defaults.valueForKey(DefaultKey.sites.rawValue) as? [[String: AnyObject]] {
+            self.sites = sites.flatMap { Site().decode($0) }
+        }
+        
+        lastViewedSiteIndex = defaults.integerForKey(DefaultKey.lastViewedSiteIndex.rawValue)
+        
+        if let uuidString = defaults.stringForKey(DefaultKey.lastViewedSiteUUID.rawValue), uuid = NSUUID(UUIDString: uuidString) {
+            lastViewedSiteUUID = uuid
+        }
+        
+        if let uuidString = defaults.stringForKey(DefaultKey.siteForComplication.rawValue), uuid = NSUUID(UUIDString: uuidString) {
+            siteForComplication = uuid
+        }
+        
+    }
+    
+    // MARK: Array modification methods
+    
     public func addSite(site: Site, atIndex: Int?) {
-        if let indexOptional = atIndex {
+        defer {
+            saveSitesToDefaults()
+        }
+        
+        if let indexOptional = atIndex where !sites.contains(site){
             if (sites.count >= indexOptional) {
                 sites.insert(site, atIndex: indexOptional )
             }
         } else {
             sites.append(site)
         }
-        
-        archiveSites()
     }
     
     public func updateSite(site: Site)  ->  Bool {
         
         defer {
-            archiveSites()
+            saveSitesToDefaults()
         }
-
+        
         if let index = sites.indexOf(site) {
             sites[index] = site
             return true
@@ -77,7 +110,10 @@ public class SitesDataSource: SitesDataSourceProvider{
     }
     
     public func removeSite(atIndex: Int) {
-        // let siteToBeRemoved = sites[index]
+        defer {
+            saveSitesToDefaults()
+        }
+        
         if atIndex <= sites.count - 1 {
             sites.removeAtIndex(atIndex)
         }
@@ -87,7 +123,5 @@ public class SitesDataSource: SitesDataSourceProvider{
             lastViewedSiteUUID = nil
             siteForComplication = nil
         }
-
-        archiveSites()
     }
 }
--- Nightscouter/SitesTableViewController.swift
+++ Nightscouter/SitesTableViewController.swift
@@ -24,7 +24,6 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
         return SitesDataSource.sharedInstance.sites
     }
     
-    
     var milliseconds: Double = 0 {
         didSet{
             let str = String(stringInterpolation:LocalizedString.lastUpdatedDateLabel.localized, AppConfiguration.lastUpdatedDateFormatter.stringFromDate(date))
@@ -61,31 +60,26 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
             let socketClient = NightscoutSocketIOClient(site: site)
             
             socketClient.mapToSite().observeNext { data in
-                
                 var site = data
-                
                 // FIXME: Configuration is not being returned for some reason... adding a default one for now.
                 if site.configuration == nil {
                     site.configuration = ServerConfiguration()
                 }
                 self.milliseconds = NSDate().timeIntervalSince1970 * 1000
                 
-                if let siteIndex = self.sites.indexOf(site) {
-                    // self.sites[siteIndex] = data
+                if let index = SitesDataSource.sharedInstance.sites.indexOf(site) {
                     SitesDataSource.sharedInstance.updateSite(site)
-                    let indexPath = NSIndexPath(forRow: siteIndex, inSection: 0)
+                    let indexPath = NSIndexPath(forRow: index, inSection: 0)
                     self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
                 } else {
                     // self.sites.append(data)
-                    SitesDataSource.sharedInstance.addSite(site, atIndex: nil)
-                    
                     let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+                    SitesDataSource.sharedInstance.addSite(site, atIndex: indexPath.row)
                     self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
                 }
+                
             }
-            
-            
-            
+            self.tableView.reloadData()
         }
         
         // Check if we should display a form.
@@ -204,6 +198,9 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
             if let selectedSiteCell = sender as? UITableViewCell {
                 let indexPath = tableView.indexPathForCell(selectedSiteCell)!
                 let selectedSite = sites[indexPath.row]
+                
+                SitesDataSource.sharedInstance.lastViewedSiteIndex = indexPath.row
+                SitesDataSource.sharedInstance.lastViewedSiteUUID = selectedSite.uuid
                 siteDetailViewController.site = selectedSite
             }
             
@@ -273,7 +270,6 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
                 editing = false
                 let newIndexPath = NSIndexPath(forRow: 0, inSection: 0)
                 SitesDataSource.sharedInstance.addSite(site, atIndex: newIndexPath.row)
-                
                 tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Automatic)
                 accessoryIndexPath = nil
             }
@@ -351,10 +347,9 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
         
         let retryAction = UIAlertAction(title: LocalizedString.generalRetryLabel.localized, style: .Default) { (action) in
             let indexPath = NSIndexPath(forRow: index, inSection: 0)
-            //let site = AppDataManageriOS.sharedInstance.sites[indexPath.row]
+            let site = SitesDataSource.sharedInstance.sites[indexPath.row]
             //site.disabled = false
-            //AppDataManageriOS.sharedInstance.updateSite(site)
-            
+            SitesDataSource.sharedInstance.updateSite(site)
             self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
         }
         alertController.addAction(retryAction)
@@ -369,7 +364,7 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
         
         let removeAction = UIAlertAction(title: LocalizedString.tableViewCellRemove.localized, style: .Destructive) { (action) in
             self.tableView.beginUpdates()
-            //AppDataManageriOS.sharedInstance.deleteSiteAtIndex(index)
+            SitesDataSource.sharedInstance.removeSite(index)
             self.tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: index, inSection: 0)], withRowAnimation: .Automatic)
             self.tableView.endUpdates()
         }
@@ -386,6 +381,4 @@ class SitesTableViewController: UITableViewController, SitesDataSourceProvider, 
             // ...
         }
     }
-    
-    
 }
